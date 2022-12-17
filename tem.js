1274eff096fe013b64bac173ab6c7897d6633d27
yuice <83380218+zyuicer@users.noreply.github.com>
2022-12-17 13:30:10 +0800

    Delete tem.js

--- tem.js
+++
@@ -1,9 +0,0 @@
-import Koa from "koa";
-import bodyParser from "koa-bodyparser";
-const app = new Koa();
-
-app.use(bodyParser());
-
-app.listen(8080, () => {
-  console.log("service run http://localhost:8080");
-});
